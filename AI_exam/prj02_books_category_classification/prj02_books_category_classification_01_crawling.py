7f01cbb483c346f2854823a5ba417cce1fd26495
makerseok <67940953+makerseok@users.noreply.github.com>
2021-06-18 08:20:17 +0900

    fix crowling code

--- AI_exam/prj02_books_category_classification/prj02_books_category_classification_01_crawling.py
+++ AI_exam/prj02_books_category_classification/prj02_books_category_classification_01_crawling.py
@@ -44,8 +44,8 @@ def get_n_books(driver: 'selenium webdriver',
                     print(book_num, end=' ')
                     dictionary['summary'].append(
                         temp.find_element_by_xpath('.//following-sibling::div[@class="Ere_prod_mconts_R"]').text)
+                    dictionary['title'].append(driver.find_element_by_xpath('//a[@class="Ere_bo_title"]').text)
             # 태그내용(=도서명) title에 저장
-            dictionary['title'].append(driver.find_element_by_xpath('//a[@class="Ere_bo_title"]').text)
 
             driver.back()
 
@@ -59,28 +59,28 @@ def get_n_books(driver: 'selenium webdriver',
 
 pages = int(10000 / 25)  # 크롤링할 페이지 수
 # cid = 1  # 이게 카테고리 (ex) 1: 소설/시/희곡, 170: 경제경영 ...
-dictionary = {'title': [], 'summary': []}
 start = time.time()  # 얼마나 걸릴지 궁금
 
 for cid in [1, 170]:  # 여기에 해당 카테고리의 cid 리스트 작성
-    for page in range(1, pages + 1):
+    dictionary = {'title': [], 'summary': []}
+    start_page = 1
+    for page in range(start_page, pages + 1):
         # page를 바꿔가며 반복
         url = f'https://www.aladin.co.kr/shop/wbrowse.aspx?BrowseTarget=List&ViewRowsCount=25&ViewType=Detail&PublishMonth=0&SortOrder=2&page={page}&Stockstatus=1&PublishDay=84&CID={cid}&SearchOption='
         print(f"\n============= {page} =============\n")
         driver.get(url)  # 해당 url로 크롬 브라우저 이동
         dictionary = get_n_books(driver, 25, dictionary)  # 해당 페이지의 도서 25권(전부임 ㅎ) 가져옴
+        # 크롤링한 데이터 데이터프레임으로 변환
+        if (page % 100 == 0 and page != 0) or page == pages:
+            data = pd.DataFrame(dictionary)
+            # 데이터프레임 및 소요시간 확인
+            print(f'time: {time.time() - start}')
 
-# 크롤링한 데이터 데이터프레임으로 변환
-print(dictionary)
-data = pd.DataFrame(dictionary)
-
-# 데이터프레임 및 소요시간 확인
-print(data)
-print(f'time: {time.time() - start}')
-
-# 장르(정답)_책 수_오늘 날짜.csv로 저장
-today = time.strftime("%Y-%m-%d", time.localtime(time.time()))
-data.to_csv(f'./crawling_data/novel_economy_{len(data)}_{today}.csv', index=False)
+            # 장-르(정답)_책 수_페이지-범위_오늘 날짜.csv로 저장
+            today = time.strftime("%Y-%m-%d", time.localtime(time.time()))
+            data.to_csv(f'./crawling_data/cid-{cid}_{len(data)}_{start_page}-{page}_{today}.csv', index=False)
+            dictionary = {'title': [], 'summary': []}
+            start_page = page + 1
 
 # 크롬 드라이버 종료
 driver.close()
