3cf2a440ed7a0a3fed0c888227e1923a4dc7a037
makerseok <maker7788@gmail.com>
2021-06-21 18:42:11 +0900

    books_category_classification commit

--- AI_exam/prj02_books_category_classification/data_concat.py
+++ AI_exam/prj02_books_category_classification/data_concat.py
@@ -5,33 +5,34 @@ from glob import glob
 import pandas as pd
 
 cid_to_cat = {
-    '1': 'Novel_Poem',
-    '170': 'Economic_Management',
-    '1230': 'Home_Cook_Beauty',
-    '1237': 'Religion_Mysticism',
-    '517': 'Art_Culture',
-    '76001' : 'Reference_Book',
-    '55890': 'Health_Hobby_Leisure',
-    '987': 'Economic',
+    "1": "Novel_Poem",
+    "170": "Economic_Management",
+    "1230": "Home_Cook_Beauty",
+    "1237": "Religion_Mysticism",
+    "517": "Art_Culture",
+    "76001": "Reference_Book",
+    "55890": "Health_Hobby_Leisure",
+    "987": "Economic",
 }
-today = time.strftime("%Y-%m-%d", time.localtime(time.time()))
+if __name__ == "__main__":
+    today = time.strftime("%Y-%m-%d", time.localtime(time.time()))
 
-filenames = glob('./crawling_data/*.csv')
-get_cid = re.compile('cid-[0-9]+')
-datas = []
+    filenames = glob("./crawling_data/*.csv")
+    get_cid = re.compile("cid-[0-9]+")
+    datas = []
 
-for filename in filenames:
-    if today in filename:
-        print(filename)
-        cid = get_cid.search(filename).group()[4:]
-        df = pd.read_csv(filename)
-        df['category'] = cid_to_cat[cid]
-        datas.append(df)
+    for filename in filenames:
+        if today in filename:
+            print(filename)
+            cid = get_cid.search(filename).group()[4:]
+            df = pd.read_csv(filename)
+            df["category"] = cid_to_cat[cid]
+            datas.append(df)
 
-data = pd.concat(datas, ignore_index=True)
+    data = pd.concat(datas, ignore_index=True)
 
-print(data.head())
-print(data.shape)
-print(data['category'].value_counts())
+    print(data.head())
+    print(data.shape)
+    print(data["category"].value_counts())
 
-data.to_csv(f'./data/raw_{len(data)}_{today}.csv', index=False)
+    data.to_csv(f"./data/raw_{len(data)}_{today}.csv", index=False)
